009350f23ca56f32a7db114ba0ef82a2a4f50156
Geethansh <geethansh@gmail.com>
2024-04-04 00:07:33 +0530

    Final

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,30 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontSize:{
+      '20': '20px',
+      '15': '15px'
+
+    },
+    
+    colors:{
+      'b-blue':'#1F3251',
+      'g-gray':'#7B879D'
+
+    },
+    backgroundColor:{
+      'card':'#ffffff'
+
+    },
     extend: {
+      fontFamily:{
+        'custom':['Outfit','sans-serif'],
+  
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
